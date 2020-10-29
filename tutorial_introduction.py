99fb8aa8112151e2cc5c6331076891615990b319
arthurtuio <arthur.henrique.2709@gmail.com>
2020-10-29 17:36:04 -0300

    added more code in tutorial_introduction

--- tutorial_introduction.py
+++ tutorial_introduction.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 
 class TutorialIntroduction:
@@ -30,7 +32,8 @@ class TutorialIntroduction:
                 "1.Tecnologias utilizadas",
                 "2.Configurando o ambiente local",
                 "3.Inscrição para a Python Brasil 2020",
-                "4.Sobre mim e meu contato"
+                "4.Um pouco do que você pode fazer com essa ferramenta",
+                "5.Sobre mim e meu contato"
             )
         )
 
@@ -43,7 +46,10 @@ class TutorialIntroduction:
         elif passo_tutorial == "3.Inscrição para a Python Brasil 2020":
             self._subscribe_python_br_2020()
 
-        elif passo_tutorial == "4.Sobre mim e meu contato":
+        elif passo_tutorial == "4.Um pouco do que você pode fazer com essa ferramenta":
+            self._example_app()
+
+        elif passo_tutorial == "5.Sobre mim e meu contato":
             self._my_contact()
 
     def _descriptions(self):
@@ -101,19 +107,17 @@ class TutorialIntroduction:
             
             Sobre a conferência:
             
-            ```
-            A Python Brasil 2020 é a maior conferência sobre linguagem de programação Python do Brasil e da América Latina.
+            *A Python Brasil 2020 é a maior conferência sobre linguagem de programação Python do Brasil e da América Latina.*
 
-            Feita pela comunidade para a comunidade, tem o objetivo de difundir a linguagem, disseminar conhecimento,
-            promover a troca de experiências e manter a comunidade crescendo igualmente em público e impacto social.
+            *Feita pela comunidade para a comunidade, tem o objetivo de difundir a linguagem, disseminar conhecimento,
+            promover a troca de experiências e manter a comunidade crescendo igualmente em público e impacto social.*
             
-            Este ano, em caráter de exceção devido à pandemia de covid-19, a Python Brasil será realizada remotamente,
+            *Este ano, em caráter de exceção devido à pandemia de covid-19, a Python Brasil será realizada remotamente,
             organizada por um time distribuído no Brasil e no mundo, a fim de prosseguir com o seu objetivo
-            de disseminar conhecimento e aproximar pessoas.
+            de disseminar conhecimento e aproximar pessoas.*
 
-            Serão sete dias de imersão, onde participantes poderão contribuir para projetos de software livre,
-            participar de treinamentos e adquirir novos conhecimentos com outras pessoas da comunidade.
-            ```
+            *Serão sete dias de imersão, onde participantes poderão contribuir para projetos de software livre,
+            participar de treinamentos e adquirir novos conhecimentos com outras pessoas da comunidade.*
             
             Para se inscrever basta acessar esse site: https://2020.pythonbrasil.org.br/
             Para ver a programação, aqui: https://2020.pythonbrasil.org.br/grade/
@@ -121,17 +125,77 @@ class TutorialIntroduction:
             **Esse tutorial será dado no domingo, dia 08, das 14h às 17h!**
         """)
 
+    def _example_app(self):
+        st.markdown("""
+            ## **4. Um pouco do que você pode fazer com essa ferramenta** ##
+            
+            Pra dar aquele gostinho do potencial da ferramenta, só clicar nas `checkboxes`
+            abaixo e ver algumas coisas do que é possível fazer :)
+        """)
+
+        if st.checkbox("Analisar tabelas/dataframes e gerar gráficos dos mesmos!"):
+            st.markdown("""
+                #### **Analisar tabelas/dataframes e gerar gráficos dos mesmos!** ####
+            
+                **Além de analisar, podemos também influenciar na criação, ou filtrar a tabela direto por aqui!**
+                
+                Como exemplo, crie uma tabela com dados aleatórios, com os dados podendo ser de 0 até um valor
+                que você define!
+            """)
+            limite_superior = st.slider(
+                "Qual o valor máximo que você quer os dados aletórios?",
+                min_value=1, max_value=100, value=1
+            )
+
+            numero_colunas = st.slider(
+                "Quantas colunas você quer que essa tabela tenha?",
+                min_value=2, max_value=5, value=2
+            )
+
+            nomes_colunas = self._create_column_names(numero_colunas)
+
+            chart_data = pd.DataFrame(
+                np.random.randint(
+                    0, limite_superior, size=(100, numero_colunas)
+                ),
+                columns=nomes_colunas
+            )
+
+            st.write("Resultado:")
+            st.dataframe(chart_data)
+
+            st.write("Vamos gerar um gráfico disso!")
+            plot_type = st.selectbox(
+                "Qual tipo de gráfico você quer?",
+                ("bar_chart", "area_chart", "line_chart")
+            )
+
+            self._make_plot(plot_type, chart_data)
+
+        if st.checkbox("Essa página!"):
+            st.markdown("""
+                #### **Essa página!** ####
+                
+                Essa página foi criada através de um código em **Python**, usando o framework **Streamlit** 
+                (que na prática é uma biblioteca para o Python), e o código da mesma pode ser visto aqui:
+                https://github.com/arthurtuio/streamlit-heroku-python-br2020/blob/master/tutorial_introduction.py
+                
+                Se você curtiu, e quer aprender mais sobre, só dar uma conferida no repositório do **github**
+                que tem esse código! E já deixo também o convite pra participares do tutorial que facilitarei 
+                na Python Brasil 2020, onde vou explicar tudo que tem lá e mais coisas!!
+            """)
+
     @staticmethod
     def _my_contact():
         st.markdown("""
-            ## **4. Sobre mim e meu contato** ##
-            
+            ## **5. Sobre mim e meu contato** ##
+
             E aí! Prazer em te conhecer! Uma introdução sobre em ~300 caracteres:
-            
+
             *Sou o Arthur Henrique, Joinvilense, amante de música e videogames, e que me considero um cara
             muito empenhado em ajudar pessoas. Já fui voluntário em vários projetos, passando por áreas bem diferentes.
             Hoje sou engenheiro de dados na Conta Azul, empresa em que aprendi muito do que sei hoje em programação.*
-            
+
             Se tiveres qualquer dúvida sobre esse tutorial, ou quiser bater um papo, pode me mandar mensagem
             nas minhas redes sociais:
             - GitHub: https://github.com/arthurtuio
@@ -181,6 +245,32 @@ class TutorialIntroduction:
             
             https://github.com/arthurtuio/streamlit-heroku-python-br-2020
         """)
+
+    @staticmethod
+    def _create_column_names(num_cols=2):
+        nome_colunas = "AB"
+
+        if num_cols == 3:
+            nome_colunas += "C"
+
+        elif num_cols == 4:
+            nome_colunas += "CD"
+
+        elif num_cols == 5:
+            nome_colunas += "CDE"
+
+        return list(nome_colunas)
+
+    @staticmethod
+    def _make_plot(plot_type, chart_data):
+        if plot_type == "bar_chart":
+            return st.bar_chart(chart_data)
+
+        elif plot_type == "area_chart":
+            return st.area_chart(chart_data)
+
+        elif plot_type == "line_chart":
+            return st.line_chart(chart_data)
 
 
 TutorialIntroduction().initialize()
